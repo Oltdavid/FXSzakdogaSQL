380b830ec3db20ed9d07148c29f4119a8e50e299
oltda <oltda@DESKTOP-RM31FLA>
2018-10-18 10:48:14 +0200

    css

--- src/sport/Sportolok.java
+++ src/sport/Sportolok.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Sportolok extends Application {
@@ -13,7 +14,7 @@ public class Sportolok extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
 
         Scene scene = new Scene(root);
-
+        Image img = new Image("file: logo.jpg", 100, 150, true, true);
         stage.setTitle("Sportolók");
         stage.setWidth(1500);
         stage.setHeight(680);
