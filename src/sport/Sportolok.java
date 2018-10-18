4dacdbf633302f42ae34fe6bb4109fb3231a4cee
oltda <oltda@DESKTOP-RM31FLA>
2018-10-18 11:41:27 +0200

    css

--- src/sport/Sportolok.java
+++ src/sport/Sportolok.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Sportolok extends Application {
@@ -12,12 +14,10 @@ public class Sportolok extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
-
         Scene scene = new Scene(root);
-        Image img = new Image("file: logo.jpg", 100, 150, true, true);
         stage.setTitle("Sportolók");
-        stage.setWidth(1500);
-        stage.setHeight(680);
+        stage.setWidth(1390);
+        stage.setHeight(650);
         stage.setScene(scene);
         stage.show();
     }
